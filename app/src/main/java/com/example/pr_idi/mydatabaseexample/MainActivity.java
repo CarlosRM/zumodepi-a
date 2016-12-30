1b244a01d731b80616091a62b65ac90a6c4e2cf9
Daryan7 <mario_bcn2@hotmail.es>
2016-12-30 16:21:13 +0100

    Vista de buscar peliculas, pero la busqueda aun no funciona

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
@@ -2,21 +2,17 @@ package com.example.pr_idi.mydatabaseexample;
 
 
 import java.util.List;
-import java.util.Random;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
     private FilmData filmData;
-    private ListView filmList;
-    private NavigationView navView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,36 +22,43 @@ public class MainActivity extends Activity {
         filmData = new FilmData(this);
         filmData.open();
 
-        List<Film> values = filmData.getAllFilms(null);
+        List<Film> values = filmData.getAllFilms(MySQLiteHelper.COLUMN_TITLE);
 
         // use the SimpleCursorAdapter to show the
         // elements in a ListView
-        filmList = (ListView)findViewById(R.id.list);
+        ListView filmList = (ListView) findViewById(R.id.list);
         ArrayAdapter<Film> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, values);
         filmList.setAdapter(adapter);
 
-        navView = (NavigationView) findViewById(R.id.navMenu);
+        NavigationView navView = (NavigationView) findViewById(R.id.navMenu);
 
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
-                    case R.id.switchActivity:
+                    case R.id.switchActivity: {
                         Intent myIntent = new Intent(getApplicationContext(), RecyclerViewActivity.class);
                         startActivity(myIntent);
                         break;
-                    case R.id.addFilmButton:
-                        Intent myIntent2 = new Intent(getApplicationContext(), InsertFilmActivity.class);
-                        startActivity(myIntent2);
+                    }
+                    case R.id.addFilmButton: {
+                        Intent myIntent = new Intent(getApplicationContext(), InsertFilmActivity.class);
+                        startActivity(myIntent);
                         break;
+                    }
+                    case R.id.searchButton: {
+                        Intent myIntent = new Intent(getApplicationContext(), SearchActivity.class);
+                        startActivity(myIntent);
+                        break;
+                    }
                     default: return false;
                 }
                 return true;
             }
         });
     }
-    
+
     @Override
     protected void onResume() {
         filmData.open();
---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/SearchActivity.java
@@ -0,0 +1,37 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private FilmData filmData;
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        filmData = new FilmData(this);
+        filmData.open();
+
+        editText = (EditText)findViewById(R.id.searchText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Toast.makeText(getApplicationContext(),"Success!",Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}

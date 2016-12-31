a1fb5a08b9136f6cddeba8cd070eed10eda24759
EnderRL <carlosroldanmontaner@gmail.com>
2016-12-31 13:19:35 +0100

    Solo me falta la busqueda wiii

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/InsertFilmActivity.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/InsertFilmActivity.java
@@ -2,6 +2,8 @@ package com.example.pr_idi.mydatabaseexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,9 +25,11 @@ public class InsertFilmActivity extends AppCompatActivity {
 
     private EditText inputTitle;
     private Spinner inputCountry;
+    private int defaultCountryValue;
     private EditText inputDirector;
     private EditText inputProtagonist;
-    private EditText inputYear;
+    private SeekBar inputYear;
+    private TextView yearValue;
     private SeekBar inputRate;
     private TextView rateValue;
     private FilmData filmData;
@@ -49,6 +53,8 @@ public class InsertFilmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insert_film);
         initializeViews();
         setListeners();
+        inputRate.setProgress(50);
+        inputCountry.setSelection(defaultCountryValue);
         filmData = new FilmData(this);
         filmData.open();
 
@@ -79,12 +85,21 @@ public class InsertFilmActivity extends AppCompatActivity {
         insertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                filmData.createFilm(inputTitle.getText().toString(), inputDirector.getText().toString(),
-                        inputCountry.getSelectedItem().toString(), inputProtagonist.getText().toString(),
-                        Integer.parseInt(inputYear.getText().toString()),
-                        inputRate.getProgress());
 
-                Toast.makeText(getApplicationContext(), "Film added successfully", Toast.LENGTH_SHORT).show();
+                if(inputTitle.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "A title is required",Toast.LENGTH_SHORT).show();
+                }else if(inputDirector.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"A director is required",Toast.LENGTH_SHORT).show();
+                }else if(inputProtagonist.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"A protagonist is required",Toast.LENGTH_SHORT).show();
+                }else {
+                    filmData.createFilm(inputTitle.getText().toString(), inputDirector.getText().toString(),
+                            inputCountry.getSelectedItem().toString(), inputProtagonist.getText().toString(),
+                            inputYear.getProgress() + 1896,
+                            inputRate.getProgress());
+
+                    Toast.makeText(getApplicationContext(), "Film added successfully", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -107,6 +122,22 @@ public class InsertFilmActivity extends AppCompatActivity {
             }
         });
 
+        inputYear.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                yearValue.setText(Integer.toString(progress+1896));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         star1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -348,12 +379,14 @@ public class InsertFilmActivity extends AppCompatActivity {
                 }
             }
             Collections.sort(countries, String.CASE_INSENSITIVE_ORDER);
+            defaultCountryValue = countries.indexOf(this.getResources().getConfiguration().locale.getDisplayCountry());
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.country_spinner_style, countries);
             inputCountry.setAdapter(adapter);
 
         inputDirector = (EditText) findViewById(R.id.insertDirectorView);
         inputProtagonist = (EditText) findViewById(R.id.insertProtagonistView);
-        inputYear = (EditText) findViewById(R.id.insertYearView);
+        inputYear = (SeekBar) findViewById(R.id.insertYearView);
+        yearValue = (TextView) findViewById(R.id.yearValueView);
         inputRate = (SeekBar) findViewById(R.id.insertRateView);
         rateValue = (TextView) findViewById(R.id.rateValueView);
         insertButton = (Button) findViewById(R.id.insertButtonView);
@@ -362,6 +395,7 @@ public class InsertFilmActivity extends AppCompatActivity {
         star3 = (ImageView) findViewById(R.id.star33);
         star4 = (ImageView) findViewById(R.id.star44);
         star5 = (ImageView) findViewById(R.id.star55);
+
         star1State = star2State = star3State = star4State = star5State = 0;
 
     }

d14d187e3f6f7f5527a3d6732e5a279941bef8ec
EnderRL <carlosroldanmontaner@gmail.com>
2017-01-07 20:31:10 +0100

    help

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/HelpActivity.java
@@ -0,0 +1,18 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        initialize_views();
+    }
+
+    private void initialize_views(){
+
+    }
+}
